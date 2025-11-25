ea679454b997d9e18e096e058c22d86070aab41d
Gaurav <gauravyadav199808@gmail.com>
2025-11-25 14:13:28 +0530

    Added Threat Detection function

---
+++ helper.py
@@ -0,0 +1,93 @@
+import requests, os, json
+
+
+def checkEnvVariable(var_name):
+    """Check if an environment variable is set and return its value."""
+    env_var  = os.environ.get(var_name)
+    if not env_var: 
+        return "Missing the environment variable: " + var_name
+    return env_var
+
+
+def analyzeThreats():
+    """
+    Analyzes system threats using the SIEM(Wazuh) data, generates AI insights on the basis of that data and performs actions if approved.
+    """
+    print("[*] Pulling FortiGate SIEM alerts from Wazuh...")
+    WAZUH_URL = checkEnvVariable("WAZUH_URL")
+    WAZUH_USER = checkEnvVariable("WAZUH_USER")
+    WAZUH_PASS = checkEnvVariable("WAZUH_PASS")
+    
+    resp = requests.post(
+        WAZUH_URL,
+        auth=(WAZUH_USER, WAZUH_PASS),
+        headers={"Content-Type": "application/json"},
+        json={
+            "query": {
+                "match": {
+                    "rule.groups": "fortigate"
+                }
+            },
+            "size": 40
+        },
+        verify=False
+    )
+
+    hits = resp.json().get("hits", {}).get("hits", [])
+    events = [hit["_source"] for hit in hits]
+
+    print(f"[+] Retrieved {len(events)} events from Wazuh SIEM")
+
+    # If no events, don’t bother calling AI
+    if not events:
+        print("[!] No FortiGate events found, exiting.")
+        exit(0)
+
+    print("[*] Sending events to AI for analysis...")
+    prompt = f"""
+    You are an AI SOC Analyst.
+
+    Here are FortiGate firewall VPN logs from a SIEM (Wazuh):
+
+    {json.dumps(events, indent=2)}
+
+    Your tasks:
+    1. Identify attack types (brute force, scanner, credential spraying, etc.)
+    2. Identify top attacker IPs and usernames.
+    3. Check for SSL alerts, IPsec tunnel failures, suspicious patterns.
+    4. Provide a risk score from 1 to 10.
+    5. Provide recommended next actions (clear and actionable).
+    6. Provide a human-readable summary for SOC team.
+
+    Respond in JSON only with keys:
+    - summary
+    - top_attackers
+    - targeted_users
+    - attack_type
+    - risk_score
+    - recommendations
+    """
+    LMAAS_URL = checkEnvVariable("LMAAS_URL")
+    LMAAS_KEY = checkEnvVariable("LMAAS_KEY")
+    MODEL = checkEnvVariable("MODEL")
+
+    print("[*] Sending logs to LiteMaaS AI model for analysis...\n")
+
+    ai_resp = requests.post(
+        LMAAS_URL,
+        headers={
+            "Authorization": f"Bearer {LMAAS_KEY}",
+            "Content-Type": "application/json"
+        },
+        json={
+            "model": MODEL,
+            "messages": [
+                {"role": "user", "content": prompt}
+            ],
+            "temperature": 0.2
+        },
+        verify=False
+    )
+
+    print("=== AI Response ===\n")
+    print(json.dumps(ai_resp.json(), indent=2))
